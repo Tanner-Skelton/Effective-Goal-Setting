34824916211201d42dc150471cddace26e8abe86
Tanner Skelton <tskelton@huskers.unl.edu>
2020-12-11 16:49:15 -0600

    Added lazy pop and a new view.

--- Goalz/Goalz/ContentView.swift
+++ Goalz/Goalz/ContentView.swift
@@ -7,10 +7,37 @@
 
 import SwiftUI
 
+struct CreateGoalView: View {
+    var body: some View{
+        NavigationView{
+            Form {
+                Text("This is the form")
+            }
+            .navigationBarTitle("Set a Goal")
+            //.navigationBarHidden(true)
+            .accentColor(.blue)
+        }
+        .lazyPop()
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            NavigationLink(
+                destination: Text("Hello World!"),
+                label: {
+                    Text("Navigate")
+                })
+                .navigationBarTitle("Goalz")
+                .toolbar(content: {
+                    NavigationLink(
+                        destination: CreateGoalView(),
+                        label: {
+                            Text("Navigate")
+                        })
+                })
+        }
     }
 }
 
