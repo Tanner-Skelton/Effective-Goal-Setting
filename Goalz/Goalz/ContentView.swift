c89b7737f358d95bb547313636b29f498c09f0e2
jerwin21 <johnerwin73@gmail.com>
2020-12-11 16:49:42 -0600

    created addGoal, deleteGoal, and saveContext in ContentView

--- Goalz/Goalz/ContentView.swift
+++ Goalz/Goalz/ContentView.swift
@@ -7,10 +7,44 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(sortDescriptors: [])
+    private var goals: FetchedResults<Goal>
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+    }
+    
+    private func addGoal() {
+        withAnimation {
+            let newGoal = Goal(context: viewContext)
+            newGoal.title = "New goal bitch"
+            newGoal.startDate = Date()
+            newGoal.endDate = nil
+            newGoal.goalDescription = "this is a new goal bitch"
+            
+            saveContext()
+        }
+    }
+    
+    private func deleteGoal(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { goals[$0]}.forEach(viewContext.delete)
+        }
+        saveContext()
+    }
+    
+    
+    private func saveContext() {
+        do {
+            try viewContext.save()
+        } catch {
+            let error = error as NSError
+            fatalError("Unresolved Error: \(error)")
+        }
     }
 }
 
