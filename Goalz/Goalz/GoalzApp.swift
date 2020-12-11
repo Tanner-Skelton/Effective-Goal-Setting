21b45021920b0f0fb305c61539370bd08ae37587
Tanner Skelton <tskelton@huskers.unl.edu>
2020-12-11 15:54:54 -0600

    Clean up post nuke.

---
+++ Goalz/Goalz/GoalzApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalzApp.swift
+//  Goalz
+//
+//  Created by Tanner Skelton on 12/11/20.
+//
+
+import SwiftUI
+
+@main
+struct GoalzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
