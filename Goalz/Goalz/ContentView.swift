e773ff8204d70ee8a969e1a54f1d465c5dfbce5e
Tanner Skelton <tskelton@huskers.unl.edu>
2020-12-13 17:11:45 -0600

    Removed un-needed code

--- Goalz/Goalz/ContentView.swift
+++ Goalz/Goalz/ContentView.swift
@@ -29,37 +29,7 @@ struct ContentView: View {
         }
     }
 }
-    /*
-    public func addGoal(title: String, startDate: Date, endDate: Date, description: String) {
-        withAnimation {
-            let newGoal = Goal(context: viewContext)
-            newGoal.title = title
-            newGoal.startDate = startDate
-            newGoal.endDate = endDate
-            newGoal.goalDescription = description
-            
-            self.saveContext()
-        }
-    }
-    
-    public func deleteGoal(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { goals[$0]}.forEach(viewContext.delete)
-        }
-        self.saveContext()
-    }
-    
-    
-    public func saveContext() {
-        do {
-            try viewContext.save()
-        } catch {
-            let error = error as NSError
-            fatalError("Unresolved Error: \(error)")
-        }
-    }
-}
-*/
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
